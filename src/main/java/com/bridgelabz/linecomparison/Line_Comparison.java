8ab83359621db01b6f1642c0886ce01ffa86ca20
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-01 13:07:11 +0530

    ADDED: Print to length of line

--- src/main/java/com/bridgelabz/linecomparison/Line_Comparison.java
+++ src/main/java/com/bridgelabz/linecomparison/Line_Comparison.java
@@ -1,2 +1,7 @@
-package com.bridgelabz.linecomparison;public class Line_Comparison {
+package com.bridgelabz.linecomparison;
+
+public class Line_Comparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
 }
